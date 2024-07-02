0ff9512f63518ded0a8d4d67d1247551f02d0567
susanw1 <susanwitts1@gmail.com>
2024-07-02 22:53:53 +0100

    More minimizing

--- src/main/java/jacksonissue/MiniModuleDef.java
+++ src/main/java/jacksonissue/MiniModuleDef.java
@@ -13,20 +13,18 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 
 public interface MiniModuleDef {
 
-    /**
-     * Most components of the Model have a name/description/longDescription - this defines them in one place.
-     */
-    interface ModelComponent {
+    interface ModuleModel {
         @JsonProperty(required = true)
         String getName();
-    }
 
-    interface ModuleModel extends ModelComponent {
         @JsonManagedReference
         List<CommandModel> getCommands();
     }
 
-    interface CommandModel extends ModelComponent {
+    interface CommandModel {
+        @JsonProperty(required = true)
+        String getName();
+
         @JsonBackReference
         ModuleModel getModule();
 
@@ -43,16 +41,9 @@ public interface MiniModuleDef {
     }
 
     interface EnumTypeDefinition extends TypeDefinition {
-        default boolean enumType() {
-            return true;
-        }
-
         List<String> getValues();
     }
 
     interface NumberTypeDefinition extends TypeDefinition {
-        default boolean numberType() {
-            return true;
-        }
     }
 }
